40460b402f1610dfccf088c7a03a8cb6a8220a92
Thomas Cavaleir <Thomas.Cavalier@Colorado.edu>
2010-11-16 16:47:43 -0700

    update pictures

---
+++ stats.php
@@ -0,0 +1,60 @@
+<html>
+<title>Footprint</title>
+<head>
+<LINK href="style2.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="header">
+<img src="img/greenfoot2.png"/>
+<h1>Footprint</h1>
+</div>
+<div id="mypageheader">
+	<a href="/">Username</a>
+	<a href="/">Blah</a>
+	<a href="/">Find Friends</a>
+	<a href="/">Logout</a>
+	<div class="clear"></div>
+</div>
+<div class="navbar">
+<div class="in">
+	<a href="/"><div class="button">Home</div></a>
+	<a href="/"><div class="button">Mypage</div></a>
+	<a href="http://foursquare.com/mobile/checkin"><div class="button">Check-In</div></a>
+	<a href="/stat.php"><div class="button">Stats</div></a>
+	<a href="/feedback.html"><div class="button">FeedBack</div></a>
+	<a href="/help.html"><div class="button">Help</div></a>
+</div>
+</div>
+<div class="userinfo">
+	<img src="img/me.jpg"/>
+	<div class="inner">
+		<h1>Username</h1><br/>
+		<div id="stat">
+			<p><b>Level:</b> 70 <b>|Rank:</b> Nature God</p>
+			<p>Stats go here.</p>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div class="achievments">
+	<h1>Achievments</h1>
+	<div class="clear"></div>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<img src="img/greenmedal.png"/><img src="img/redmedal.png"/><img src="img/bluemedal.png"/>
+		<div class="clear"></div>
+</div>
+<div class="footprint">
+	<td><img src="img/shoe.png"/> <img src="img/bus.png"/> <img src="img/car.png"/><img src="img/run.png"/><img src="img/shoe.png"/> <img src="img/bus.png"/> <img src="img/car.png"/><img src="img/run.png"/></td>
+</div>
+</body>
+</html>
