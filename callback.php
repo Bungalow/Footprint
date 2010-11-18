343a60b686d2e889b1d4cfc32421973229c74c36
Thomas Cavaleir <Thomas.Cavalier@Colorado.edu>
2010-11-17 23:58:13 -0700

    fix my callback.php

--- callback.php
+++ callback.php
@@ -1,110 +1,110 @@
-
 <?php
-	require_once('init.php');
-	
-	$foursquareObj = new EpiFoursquare($consumer_key, $consumer_secret);
-	
-	if (!isset($_SESSION['oauth_token'])) {
-		$foursquareObj->setToken($_REQUEST['oauth_token'],$_SESSION['secret']);
-		$token = $foursquareObj->getAccessToken();
-		$_SESSION['oauth_token'] = $token->oauth_token;
-		$_SESSION['secret'] = $token->oauth_token_secret;
-	}
-	
-	$foursquareObj->setToken($_SESSION['oauth_token'], $_SESSION['secret']);
-	
-	// User info
-	$params = array("badges"=>1, "mayor"=>1);
-	$foursquareUser = $foursquareObj->get_user($params);
-	$user_info = $foursquareUser->response['user'];
-	$_SESSION['currentUserID'] = $user_info['id'];
-	$_SESSION['4sq_user'] = $user_info;
-	$_SESSION['4sq_obj'] = $foursquareObj;
+require_once('init.php');
 
-	try {
-		 
-		// alert if homebase isn't set -> should do this during registration -> after registration, but before page access -> shouldn't be able to get to this page without homebase being set
-		
-		// what if homebase isn't a foursquare location?
-		/* -> can guide user through creation of venue (cumbersome)
-		 * -> can set foursquare vid to 0 or -1 and set geolocation (geolong, geolat) via google maps
-		 */
-		
-		//ideally, these items should be stored in the Footprint db to save on 4sq API calls
-		//hardcoded homebase
-		$homebase = $foursquareObj->get_venue(array("vid"=>$homebase_vid));
-		// $homebaseVID = 4500522; this should now be instantiated in config.php
-		$homebaseGeoLat = $homebase->venue->geolat;
-		$homebaseGeoLong = $homebase->venue->geolong;
-		$homebaseURL = $homebase->venue->short_url;
-		$_SESSION["homebase"] = Array(
-			"geolat" => $homebaseGeoLat,
-			"geolong" => $homebaseGeoLong
-		)
-		//another reason to store these items in the db	is for homebases that aren't 4sq locationss
-		
-		//eventually, this will be done via async js/php calls - for now, we'll cheat
-		//specifying the variables like this essentially makes them global variables
-		//(i know, i know - like i said, cheating)
+$foursquareObj = new EpiFoursquare($consumer_key, $consumer_secret);
+
+if (!isset($_SESSION['oauth_token'])) {
+$foursquareObj->setToken($_REQUEST['oauth_token'],$_SESSION['secret']);
+$token = $foursquareObj->getAccessToken();
+$_SESSION['oauth_token'] = $token->oauth_token;
+$_SESSION['secret'] = $token->oauth_token_secret;
+}
+
+$foursquareObj->setToken($_SESSION['oauth_token'], $_SESSION['secret']);
+
+// User info
+$params = array("badges"=>1, "mayor"=>1);
+$foursquareUser = $foursquareObj->get_user($params);
+$user_info = $foursquareUser->response['user'];
+$_SESSION['currentUserID'] = $user_info['id'];
+$_SESSION['4sq_user'] = $user_info;
+$_SESSION['4sq_obj'] = $foursquareObj;
+
+try {
+
+// alert if homebase isn't set -> should do this during registration -> after registration, but before page access -> shouldn't be able to get to this page without homebase being set
+
+// what if homebase isn't a foursquare location?
+/* -> can guide user through creation of venue (cumbersome)
+* -> can set foursquare vid to 0 or -1 and set geolocation (geolong, geolat) via google maps
+*/
+
+//ideally, these items should be stored in the Footprint db to save on 4sq API calls
+//hardcoded homebase
+$homebase = $foursquareObj->get_venue(array("vid"=>$homebase_vid));
+// $homebaseVID = 4500522; this should now be instantiated in config.php
+$homebaseGeoLat = $homebase->venue->geolat;
+$homebaseGeoLong = $homebase->venue->geolong;
+$homebaseURL = $homebase->venue->short_url;
+$_SESSION["homebase"] = Array(
+"geolat" => $homebaseGeoLat,
+"geolong" => $homebaseGeoLong
+)
+//another reason to store these items in the db is for homebases that aren't 4sq locationss
+
+//eventually, this will be done via async js/php calls - for now, we'll cheat
+//specifying the variables like this essentially makes them global variables
+//(i know, i know - like i said, cheating)
 ?>
 
-<?php		
-		//see if there are any records in the db
-		//order by timestamp desc
-		
-		//if there are, get the most recent record's timestamp and set it as sinceid as option in history query
-		
-		$destinationsQuery = mysql_query("select foursquarecheckinid from destinations where userid=".$_SESSION['currentUserID']." order by foursquarecheckinid desc");
-		if (mysql_num_rows($destinationsQuery) == 0) {
-			$sinceID = "";
-		}
-		else {
-			$resultRow = mysql_fetch_object($destinationsQuery);
-			$sinceID = $resultRow->foursquarecheckinid;
-		}
-		
-		//get recent checkins
-		//$venues = $foursquareObj->get_history();
-		$venues = $foursquareObj->get_history(array("sinceid"=>$sinceID))->checkins;
-		
-		//a cleaner approach: filter out 'shout-only' checkins from $venues so they don't show up in the total number of venues
-		
-		$numVenues = sizeof($venues);
-		//echo("Number of new stops: ".$numVenues."<br />");
-		//if using a sinceid, ordering will be oldest to newest. if not, ordering will be newest to oldest
-		if ($sinceID != "") {
-			$venues = array_reverse($venues);
-		}
-		
-		//insert data from foursquare into database
-		for ($i=0; $i<$numVenues; $i++) {
-			//need to check each venue to make sure it is a legal venue
-			$currentVenue = ($venues[$i]);
-			if (isset($currentVenue->venue->id)) {
-				$currentVID = $currentVenue->venue->id;
-				$currentVenueCheckinTime = $currentVenue->created;
-				$currentVenueLongURL = "http://foursquare.com/venue/".$currentVID;
-				$currentVenueName = htmlspecialchars($currentVenue->venue->name, ENT_QUOTES);
-				$currentFoursquareCheckinID = $currentVenue->id;
-				$currentVenueGeoLat = $currentVenue->venue->geolat;
-				$currentVenueGeoLong = $currentVenue->venue->geolong;
-				//insert data into database
+<?php
+//see if there are any records in the db
+//order by timestamp desc
 
-			}
-		}
-	}
+//if there are, get the most recent record's timestamp and set it as sinceid as option in history query
 
-	catch (Exception $e) {
-		echo("Well, golly - something went wrong.<br />");
-		echo "Error: " . $e;
- 	}
+$destinationsQuery = mysql_query("select foursquarecheckinid from destinations where userid=".$_SESSION['currentUserID']." order by foursquarecheckinid desc");
+if (mysql_num_rows($destinationsQuery) == 0) {
+$sinceID = "";
+}
+else {
+$resultRow = mysql_fetch_object($destinationsQuery);
+$sinceID = $resultRow->foursquarecheckinid;
+}
+
+//get recent checkins
+//$venues = $foursquareObj->get_history();
+$venues = $foursquareObj->get_history(array("sinceid"=>$sinceID))->checkins;
+
+//a cleaner approach: filter out 'shout-only' checkins from $venues so they don't show up in the total number of venues
+
+$numVenues = sizeof($venues);
+//echo("Number of new stops: ".$numVenues."<br />");
+//if using a sinceid, ordering will be oldest to newest. if not, ordering will be newest to oldest
+if ($sinceID != "") {
+$venues = array_reverse($venues);
+}
+
+//insert data from foursquare into database
+for ($i=0; $i<$numVenues; $i++) {
+//need to check each venue to make sure it is a legal venue
+$currentVenue = ($venues[$i]);
+if (isset($currentVenue->venue->id)) {
+$currentVID = $currentVenue->venue->id;
+$currentVenueCheckinTime = $currentVenue->created;
+$currentVenueLongURL = "http://foursquare.com/venue/".$currentVID;
+$currentVenueName = htmlspecialchars($currentVenue->venue->name, ENT_QUOTES);
+$currentFoursquareCheckinID = $currentVenue->id;
+$currentVenueGeoLat = $currentVenue->venue->geolat;
+$currentVenueGeoLong = $currentVenue->venue->geolong;
+//insert data into database
+$result = mysql_query("insert into destinations (userid, foursquarevid, checkintime, foursquareurl, geolat, geolong, foursquarecheckinid, venuename) values ('".$_SESSION['currentUserID']."', '".$currentVID."', '".$currentVenueCheckinTime."', '".$currentVenueLongURL."', '".$currentVenueGeoLat."', '".$currentVenueGeoLong."', '".$currentFoursquareCheckinID."', '".$currentVenueName."')");
+//echo("number of affected rows:".mysql_num_rows($result));
+}
+}
+}
+
+catch (Exception $e) {
+echo("Well, golly - something went wrong.<br />");
+echo "Error: " . $e;
+  }
 
 ?>
 <html>
-	<head>
-		<meta http-equiv="refresh" content="0;url=/">
-	</head>
-	<body>
-		Redirecting
-	</body>
+<head>
+<meta http-equiv="refresh" content="0;url=/">
+</head>
+<body>
+Redirecting
+</body>
 </html>
--- index.php
+++ index.php
@@ -4,188 +4,184 @@ tmpl('header');
 ?>
 
 <?php if (!$logged_in) { // no user ?>
-	
-	<div class="login">
-		<img src="img/greenlogin.png"/>
-		<p>
-			<?php
-	
-				try {
-				  $foursquareObj = new EpiFoursquare($consumer_key, $consumer_secret);
-				  $results = $foursquareObj->getAuthorizeUrl();
-				  $loginurl = $results['url'] . "?oauth_token=" . $results['oauth_token'];
-				  $_SESSION['secret'] = $results['oauth_token_secret'];
-				}
-	
-				catch (Execption $e) {
-					echo("here is the exception: ".print_r($e));
-				  //If there is a problem throw an exception
-				}
-				echo "<a href='" . $loginurl . "'><input type='button' name='groovybtn2' class='groovy' value='Login Via FourSquare' title=''></a>";  //Display the Foursquare login link
-			?>
-		</p>
-		<p>Not a FourSquare User? It's ok join by clicking below:</p>
-		<img src="img/greennew.png"/><br/>
-		<a href="http://foursquare.com"><input type="button" name="groovybtn2" class="groovy" value="Join Foursquare" title=""></a>
-	</div>
-	
+
+<div class="login">
+<img src="img/greenlogin.png"/>
+<p>
+<?php
+
+try {
+$foursquareObj = new EpiFoursquare($consumer_key, $consumer_secret);
+$results = $foursquareObj->getAuthorizeUrl();
+$loginurl = $results['url'] . "?oauth_token=" . $results['oauth_token'];
+$_SESSION['secret'] = $results['oauth_token_secret'];
+}
+
+catch (Execption $e) {
+echo("here is the exception: ".print_r($e));
+//If there is a problem throw an exception
+}
+echo "<a href='" . $loginurl . "'><input type='button' name='groovybtn2' class='groovy' value='Login Via FourSquare' title=''></a>"; //Display the Foursquare login link
+?>
+</p>
+<p>Not a FourSquare User? It's ok join by clicking below:</p>
+<img src="img/greennew.png"/><br/>
+<a href="http://foursquare.com"><input type="button" name="groovybtn2" class="groovy" value="Join Foursquare" title=""></a>
+</div>
 <?php }else{ // user is logged in ?>
-	
-	<?php
-	//get mileage info
-	$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='self'");
-	$selfPowMileage = round(mysql_fetch_object($result)->mileagesum,2);
-	$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='mass'");
-	$massTransMileage = round(mysql_fetch_object($result)->mileagesum,2);
-	$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='car'");
-	$carMileage = round(mysql_fetch_object($result)->mileagesum,2);
-	$totalMileage = $selfPowMileage + $massTransMileage + $carMileage;
-	?>
-	
-	<div class="userinfo">
-		<img src="img/me.jpg"/>
-		<div class="inner">
-			<h1><?php echo $user_info['firstname']; ?> <?php echo $user_info['lastname']; ?></h1><br/>
-			<div id="stat">
-				<p><b>Level:</b> 70 <b>Rank:</b> Nature God</p>
-			</div>
-			<div class="clear"></div>
-		</div>
-	</div>
-	<div class="achievments">
-		<h1>Achievments</h1>
-		<div class="clear"></div>
-		<?php
-		foreach($user_info['badges'] as $badge){
-		?>
-		<img src="<?php echo $badge['icon']; ?>"/>
-		<?php
-		}
-		?>	
-		<div class="clear"></div>
-	</div>
-	
-	<div class="footprint">
-		<h2>Your Footprint:</h2>
-		<em>How you get to where you go affects the environment.</em>
-		<div id="headerInfo">
-			<div id="mileageDetails">
-				<div class='headerRow'>Total Miles: <span id='totalMileage' class='mileageSum'><?php echo($totalMileage);?></span></div>
-				<div class='headerRow'>Self-powered Miles: <span id='selfPowMileage' class='mileageSum'><?php echo($selfPowMileage);?></span></div>
-				<div class='headerRow'>Mass Transit Miles: <span id='massTransitMileage' class='mileageSum'><?php echo($massTransMileage);?></span></div>
-				<div class='headerRow'>Driving Miles: <span id='carMileage' class='mileageSum'><?php echo($carMileage);?></span></div>
-			</div>
-			<div id="accomplishmentsCont">
-				<div class="accomplishment">
-					<?php
-						/* this messaging should be dynamic based on heuristics that we specify
-						 * 
-						 */
-					?>
-					In the past 14 days, you've racked up <span class='mileageSum'><?php echo($selfPowMileage);?></span> miles on foot, which is <span class='mileageSum'>##</span> more than your previous 14 day total.  Nice work!
-				</div>
-			</div>
-			<br clear="all" />
-		</div>
-	<script type='text/javascript' language='JavaScript'>
-		var homebaseGeoLat = <?php echo($_SESSION["homebase"]["geolat"]); ?>;
-		var homebaseGeoLong = <?php echo($_SESSION["homebase"]["geolat"]); ?>; 
-	</script>
-	<?php		
-		
-		//this data will come from the application database
-		//change numVenues to a default (20)
-			$result = mysql_query("select * from destinations where userid='".$_SESSION['currentUserID']."' order by foursquarecheckinid desc limit 25");
-			$numRows = mysql_num_rows($result);
-			if ($numRows > 0) {
-			//if no results, don't display table headers
-?>	
-				<table id="destinationTable">
-					<thead>
-						<tr>
-							<th>Destination</th>
-							<th id="checkInDateCol">Check In Date</th>
-							<th id="startPointCol">Start Point</th>
-							<th id="mileageCol">Mileage</th>
-							<th>Mode of Transportation</th>
-							<th class="lastStopCell">Last Stop?</th>
-							<th class="ignoreCheckInCell">Ignore Check In?</th>  <?php  //or would deleting the record be better? ?>
-							<th id="updateCol">&nbsp;</th>
-						</tr>
-					</thead>
-				<tbody>
+
 <?php
-				/* need to store mileage in db */
-				
-				for ($i=0; $i<$numRows; $i++) {
-					//need to check each venue to make sure it is a legal venue
-					$currentVenue = mysql_fetch_object($result);
-					if (isset($currentVenue->foursquarecheckinid)) {
-						//$currentVID = $currentVenue->foursquarevid;
-						$currentVenueLongURL = "http://foursquare.com/venue/".$currentVenue->foursquarevid;
-						
-						//print_r($currentVenueDetails);
-						//echo("venue name: ".$currentVenue->venue->name." (".$currentVenue->created.")<br />");
-						echo("<tr>\n<td>");
-						
-						//if ($currentVenueLongURL != "") {
-						echo("<a href='".$currentVenueLongURL."' target='_blank'>".$currentVenue->venuename."</a><input type='hidden' class='foursquareCheckInID' value='".$currentVenue->foursquarecheckinid."' />");
-						//}
-						//else {
-						//	echo(htmlspecialchars($currentVenue->venue->name));
-						//}
-						
-						echo("</td>\n");
-						echo("<td>".parseFoursquareDate($currentVenue->checkintime)."</td>\n");
-						//echo("<td></td>\n");
-						//echo("<td>".$currentVenue->id."</td>\n");
-						
-						/* 
-						 * need to  get vid of next venue in db (previous in timeline) ->? why?
-						 * 
-						 */
-						
-						//if ignoreChecked, select boxes and last stop should be disabled
-						if ($currentVenue->ignorecheckin == 1) {
-							$ignoreChecked = " checked='checked'";
-							$startPointDisabled = $transportModeDisabled = $lastStopDisabled = " disabled='disabled'";
-						}
-						else {
-							$ignoreChecked = $startPointDisabled = $lastStopDisabled = $transportModeDisabled = "";
-						}
-						
-						//$ignoreChecked = ($currentVenue->ignorecheckin == 1 ? " checked='checked'" : "");
-						
-						
-						echo("<td><input type='hidden' class='geoLatCoord' value='".$currentVenue->geolat."' /><input type='hidden' class='geoLongCoord' value='".$currentVenue->geolong."' />");
-						echo("<select class='startPointSelector'".$startPointDisabled."><option value='-1'>select start point...</option>");
-						echo("<option value='home'".selectValueChecker($currentVenue->startpoint, "home").">Homebase</option>");
-						echo("<option value='prev'".selectValueChecker($currentVenue->startpoint, "prev").">Previous Check In</option></select></td>\n");
+//get mileage info
+$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='self'");
+$selfPowMileage = round(mysql_fetch_object($result)->mileagesum,2);
+$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='mass'");
+$massTransMileage = round(mysql_fetch_object($result)->mileagesum,2);
+$result = mysql_query("select sum(mileage) as mileagesum from destinations where userid='".$_SESSION['currentUserID']."' and transportmode='car'");
+$carMileage = round(mysql_fetch_object($result)->mileagesum,2);
+$totalMileage = $selfPowMileage + $massTransMileage + $carMileage;
+?>
+<div class="userinfo">
+<img src="img/me.jpg"/>
+<div class="inner">
+<h1><?php echo $user_info['firstname']; ?> <?php echo $user_info['lastname']; ?></h1><br/>
+<div id="stat">
+<p><b>Level:</b> 70 <b>Rank:</b> Nature God</p>
+</div>
+<div class="clear"></div>
+</div>
+</div>
+<div class="achievments">
+<h1>Achievments</h1>
+<div class="clear"></div>
+<?php
+foreach($user_info['badges'] as $badge){
+?>
+<img src="<?php echo $badge['icon']; ?>"/>
+<?php
+}
+?>
+<div class="clear"></div>
+</div>
+<div class="footprint">
+<h2>Your Footprint:</h2>
+<em>How you get to where you go affects the environment.</em>
+<div id="headerInfo">
+<div id="mileageDetails">
+<div class='headerRow'>Total Miles: <span id='totalMileage' class='mileageSum'><?php echo($totalMileage);?></span></div>
+<div class='headerRow'>Self-powered Miles: <span id='selfPowMileage' class='mileageSum'><?php echo($selfPowMileage);?></span></div>
+<div class='headerRow'>Mass Transit Miles: <span id='massTransitMileage' class='mileageSum'><?php echo($massTransMileage);?></span></div>
+<div class='headerRow'>Driving Miles: <span id='carMileage' class='mileageSum'><?php echo($carMileage);?></span></div>
+</div>
+<div id="accomplishmentsCont">
+<div class="accomplishment">
+<?php
+/* this messaging should be dynamic based on heuristics that we specify
+*
+*/
+?>
+In the past 14 days, you've racked up <span class='mileageSum'><?php echo($selfPowMileage);?></span> miles on foot, which is <span class='mileageSum'>##</span> more than your previous 14 day total. Nice work!
+</div>
+</div>
+<br clear="all" />
+</div>
+<script type='text/javascript' language='JavaScript'>
+var homebaseGeoLat = <?php echo($_SESSION["homebase"]["geolat"]); ?>;
+var homebaseGeoLong = <?php echo($_SESSION["homebase"]["geolat"]); ?>;
+</script>
+<?php
 
-						echo("<td class='mileageCell'>".round($currentVenue->mileage,2)."</td>\n");
-						
-						echo("<td><select class='transportationModeSelector'".$transportModeDisabled."><option value='0'>select transportation...</option>");
-						echo("<option value='self'".selectValueChecker($currentVenue->transportmode, "self").">Self-Powered</option>");
-						echo("<option value='mass'".selectValueChecker($currentVenue->transportmode, "mass").">Mass Transportation</option>");
-						echo("<option value='car'".selectValueChecker($currentVenue->transportmode, "car").">Car</option></select></td>\n");
+//this data will come from the application database
+//change numVenues to a default (20)
+$result = mysql_query("select * from destinations where userid='".$_SESSION['currentUserID']."' order by foursquarecheckinid desc limit 25");
+$numRows = mysql_num_rows($result);
+if ($numRows > 0) {
+//if no results, don't display table headers
+?>
+<table id="destinationTable">
+<thead>
+<tr>
+<th>Destination</th>
+<th id="checkInDateCol">Check In Date</th>
+<th id="startPointCol">Start Point</th>
+<th id="mileageCol">Mileage</th>
+<th>Mode of Transportation</th>
+<th class="lastStopCell">Last Stop?</th>
+<th class="ignoreCheckInCell">Ignore Check In?</th> <?php //or would deleting the record be better? ?>
+<th id="updateCol">&nbsp;</th>
+</tr>
+</thead>
+<tbody>
+<?php
+/* need to store mileage in db */
 
-						$lastStopChecked = ($currentVenue->laststop == 1 ? " checked='checked'" : ""); 
-						echo("<td class='lastStopCell'><input type='checkbox' class='lastStopCheckbox'".$lastStopChecked.$lastStopDisabled." /></td>\n");
-						
-						echo("<td class='ignoreCheckInCell'><input type='checkbox' class='ignoreCheckinCheckbox'".$ignoreChecked." /></td>\n");
-						echo("<td><input type='button' value='update' class='updateDestinationInfoButton' /><span class='progressIndicator'>&nbsp;</span></td>");
-						//echo("<td class='rowStatus'>&nbsp;</td>");	//used to display update status messaging
-						echo("</tr>\n");
-					}
-				}
+for ($i=0; $i<$numRows; $i++) {
+//need to check each venue to make sure it is a legal venue
+$currentVenue = mysql_fetch_object($result);
+if (isset($currentVenue->foursquarecheckinid)) {
+//$currentVID = $currentVenue->foursquarevid;
+$currentVenueLongURL = "http://foursquare.com/venue/".$currentVenue->foursquarevid;
+
+//print_r($currentVenueDetails);
+//echo("venue name: ".$currentVenue->venue->name." (".$currentVenue->created.")<br />");
+echo("<tr>\n<td>");
+
+//if ($currentVenueLongURL != "") {
+echo("<a href='".$currentVenueLongURL."' target='_blank'>".$currentVenue->venuename."</a><input type='hidden' class='foursquareCheckInID' value='".$currentVenue->foursquarecheckinid."' />");
+//}
+//else {
+// echo(htmlspecialchars($currentVenue->venue->name));
+//}
+
+echo("</td>\n");
+echo("<td>".parseFoursquareDate($currentVenue->checkintime)."</td>\n");
+//echo("<td></td>\n");
+//echo("<td>".$currentVenue->id."</td>\n");
+
+/*
+* need to get vid of next venue in db (previous in timeline) ->? why?
+*
+*/
+
+//if ignoreChecked, select boxes and last stop should be disabled
+if ($currentVenue->ignorecheckin == 1) {
+$ignoreChecked = " checked='checked'";
+$startPointDisabled = $transportModeDisabled = $lastStopDisabled = " disabled='disabled'";
+}
+else {
+$ignoreChecked = $startPointDisabled = $lastStopDisabled = $transportModeDisabled = "";
+}
+
+//$ignoreChecked = ($currentVenue->ignorecheckin == 1 ? " checked='checked'" : "");
+
+
+echo("<td><input type='hidden' class='geoLatCoord' value='".$currentVenue->geolat."' /><input type='hidden' class='geoLongCoord' value='".$currentVenue->geolong."' />");
+echo("<select class='startPointSelector'".$startPointDisabled."><option value='-1'>select start point...</option>");
+echo("<option value='home'".selectValueChecker($currentVenue->startpoint, "home").">Homebase</option>");
+echo("<option value='prev'".selectValueChecker($currentVenue->startpoint, "prev").">Previous Check In</option></select></td>\n");
+
+echo("<td class='mileageCell'>".round($currentVenue->mileage,2)."</td>\n");
+
+echo("<td><select class='transportationModeSelector'".$transportModeDisabled."><option value='0'>select transportation...</option>");
+echo("<option value='self'".selectValueChecker($currentVenue->transportmode, "self").">Self-Powered</option>");
+echo("<option value='mass'".selectValueChecker($currentVenue->transportmode, "mass").">Mass Transportation</option>");
+echo("<option value='car'".selectValueChecker($currentVenue->transportmode, "car").">Car</option></select></td>\n");
+
+$lastStopChecked = ($currentVenue->laststop == 1 ? " checked='checked'" : "");
+echo("<td class='lastStopCell'><input type='checkbox' class='lastStopCheckbox'".$lastStopChecked.$lastStopDisabled." /></td>\n");
+
+echo("<td class='ignoreCheckInCell'><input type='checkbox' class='ignoreCheckinCheckbox'".$ignoreChecked." /></td>\n");
+echo("<td><input type='button' value='update' class='updateDestinationInfoButton' /><span class='progressIndicator'>&nbsp;</span></td>");
+//echo("<td class='rowStatus'>&nbsp;</td>"); //used to display update status messaging
+echo("</tr>\n");
+}
+}
 ?>
 
-					</tbody>
-				</table>
+</tbody>
+</table>
 <?php
-			}
+}
 ?>
-	
 <?php } ?>
 
 <?php tmpl('footer'); ?>
